a327758b387f3da9b574e54cb73053c48637c380
zawislakm <maksymilian.zawislak@gmail.com>
2024-03-03 15:45:28 +0100

    TCP && UDP work better, better handle

--- Lab1/homework/Client.py
+++ Lab1/homework/Client.py
@@ -1,44 +1,39 @@
 import re
 import socket
+import sys
 import threading
 
-server_host = '127.0.0.1'
-server_port = 8822
+SERVER_HOST = '127.0.0.1'
+SERVER_PORT = 8837
 
 MULTICAST_GROUP = '224.1.1.1'
 MULTICAST_PORT = 5007
 
-
 # TODO multicast
 
-def receive_messages(client_socket: socket):
-    print("TCP receiving messages from server thread started")
+
+server_open = threading.Event()
+
+
+def receive_messages(client_socket: socket, communication_type: str):
+    print(f"Receiving messages from server thread started, communication type: {communication_type}")
+
     try:
-        while True:
+        while not server_open.is_set():
             data = client_socket.recv(1024)
+
             if not data:
-                print("breaking, server disconnected?")
-                break
+                server_open.set()
+                raise OSError
             print(data.decode())
 
-    except KeyboardInterrupt:
-        print("Client shutting down")
-        client_socket.close()
+        else:
+            print("Client shutting down")
 
+    except (socket.error, socket.timeout, OSError):
+        print("Server disconnected")
 
-def udp_receive_messages(client_socket: socket):
-    print("UPD receiving messages from server thread started")
-    try:
-        while True:
-            data = client_socket.recvfrom(1024)
-            if not data:
-                print("breaking, server disconnected?")
-                break
-            print(data[0].decode())
-
-    except KeyboardInterrupt:
-        print("Client shutting down")
-        client_socket.close()
+    print(f"Receiving messages from server thread ended, communication type: {communication_type}")
 
 
 def get_client_id(tcp_client_socket: socket) -> int | None:
@@ -55,10 +50,8 @@ def get_client_id(tcp_client_socket: socket) -> int | None:
     return client_id
 
 
-def udp_communication(client_id: int, udp_socket: socket):
-    global server_host, server_port
-
-    print("UDP communication started")
+def udp_communication(client_id: int, udp_socket: socket, host: str, port: int, communication_type: str):
+    print(f"Communication type: {communication_type} started")
     try:
         while True:
             message = ""
@@ -71,60 +64,89 @@ def udp_communication(client_id: int, udp_socket: socket):
 
             if message == "Q\n":
                 break
+            if message == "\n":
+                continue
 
             new_message = f"{client_id}:{message}"
-            udp_socket.sendto(new_message.encode('utf-8'), (server_host, server_port))
+            udp_socket.sendto(new_message.encode('utf-8'), (host, port))
 
     except KeyboardInterrupt:
-        print("Client shutting down")
-        udp_socket.close()
-
-    print("UDP communication ended")
+        pass
+    print(f"Communication type: {communication_type} ended")
 
 
-def send_messages(tcp_socket: socket, udp_socket: socket, client_id: int):
-    print("Sending messages to server thread started")
+def main():
+    global SERVER_HOST, SERVER_PORT, MULTICAST_GROUP, MULTICAST_PORT
 
+    # Create a TCP socket
+    tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcp_socket.connect((SERVER_HOST, SERVER_PORT))
+
+    client_id = get_client_id(tcp_socket)
+
+    # Create a UDP socket
+    msg = f"{client_id}: UDP communication started from client {client_id}"
+    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    udp_socket.sendto(msg.encode(), (SERVER_HOST, SERVER_PORT))
+
+    # Create a Multicast socket
+
+    # send_mess_thread = threading.Thread(target=send_messages, args=(
+    #     tcp_socket, udp_socket, client_id,))
+    # send_mess_thread.start()
+
+    tcp_recv_thread = threading.Thread(target=receive_messages, args=(tcp_socket, "TCP",))
+    udp_recv_thread = threading.Thread(target=receive_messages, args=(udp_socket, "UDP",))
+    # multicast_recv_thread = threading.Thread(target=receive_messages, args=(multicast_socket, "Multicast",))
+
+    tcp_recv_thread.start()
+    udp_recv_thread.start()
+    # multicast_recv_thread.start()
+
+    print("Sending messages to server started")
+
+    print("-" * 100)
     try:
-        while True:
+        while not server_open.is_set():
             message = input("")
+            if message == "Q":
+                print("Client quiting server")
+                break
 
             if message == "U":
-                udp_communication(client_id, udp_socket)
+                udp_communication(client_id, udp_socket, SERVER_HOST, SERVER_PORT, "UDP")
+                continue
+            if message == "M":
+                udp_communication(client_id, udp_socket, MULTICAST_GROUP, MULTICAST_PORT, "Multicast")
                 continue
 
             tcp_socket.sendall(message.encode())
 
+    except (socket.error, socket.timeout):
+        print("Server disconnected")
+
     except KeyboardInterrupt:
         print("Client shutting down")
-        udp_socket.close()
+
+    finally:
+        server_open.set()
+
+        try:
+            print("Shutting down sockets")
+            tcp_socket.shutdown(socket.SHUT_RDWR)
+            udp_socket.shutdown(socket.SHUT_RDWR)
+        except OSError:
+            pass
+
         tcp_socket.close()
+        udp_socket.close()
 
+        udp_recv_thread.join()
+        tcp_recv_thread.join()
+        # multicast_recv_thread.join()
 
-def main():
-    # Server host and port
-
-    # Create a TCP socket
-    tcp_client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    tcp_client_socket.connect((server_host, server_port))
-
-    client_id = get_client_id(tcp_client_socket)
-
-    # Create a UDP socket
-    msg = f"{client_id}: UDP communication started from client {client_id}"
-    udp_client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    udp_client_socket.sendto(msg.encode(), (server_host, server_port))
-
-    # Create a Multicast socket
-
-    send_mess_thread = threading.Thread(target=send_messages, args=(tcp_client_socket, udp_client_socket, client_id,))
-    send_mess_thread.start()
-
-    tcp_recv_thread = threading.Thread(target=receive_messages, args=(tcp_client_socket,))
-    udp_recv = threading.Thread(target=udp_receive_messages, args=(udp_client_socket,))
-
-    udp_recv.start()
-    tcp_recv_thread.start()
+        print("Client ended")
+        sys.exit(0)
 
 
 if __name__ == "__main__":
--- Lab1/homework/Server.py
+++ Lab1/homework/Server.py
@@ -3,8 +3,10 @@ import sys
 import threading
 
 host = '127.0.0.1'
-port = 8822
+port = 8837
 max_users = 5
+
+exit_flag = threading.Event()
 
 
 class Client:
@@ -29,15 +31,18 @@ class Client:
                 break
             # Echo the received data back to the client
             # client_socket.sendall(data)
-            print(f"Received from {self.tcp_socket}: {data.decode()}")
-            new_message = f"Client {self.id}:\n{data.decode()} from TCP"
-            print("TCP SERVER: ", new_message)
+            new_message = f"T{self.id}:{data.decode()}"
             self.send_to_all_clients(new_message.encode())
 
+        if self.active is not False:
+            print(f"Client {self.id} disconnected")
+
         # Close the client socket
+        print(f"Connection with {self.tcp_socket} closed")
+
         self.tcp_socket.close()
-        print(f"Connection from {self.tcp_socket} closed")
         Client.instances.pop(self.id)
+        print(f"Connection with {self.id} closed")
         del self
         sys.exit(0)
 
@@ -60,57 +65,72 @@ class Client:
             return
 
     def send_to_client_udp(self, message: bytes):
-
         with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-            print(f"Sending message to {self.udp_address}", type(self.udp_address))
             s.sendto(message, self.udp_address)
 
-    # method to remove instance after client disconnects
 
     @classmethod
     def get_udp_address(cls) -> set:
         return set([client.udp_address for client in Client.instances.values() if client.udp_address is not None])
 
+    @classmethod
+    def get_clients_sockets(cls) -> list:
+        return [client.tcp_socket for client in Client.instances.values() if client.tcp_socket is not None]
+
 
 def handle_udp_communication(udp_socket: socket):
-    print(f"Accepted UDP connection from {socket.gethostbyname(socket.gethostname())}")
+    print("Sever UDP communication handler running...")
+    try:
+        while not exit_flag.is_set():
 
-    while True:
-        data, address = udp_socket.recvfrom(1024)
+            data, address = udp_socket.recvfrom(1024)
 
-        client_id, message = data.decode().split(":")
-        client = Client.instances.get(int(client_id))
+            client_id, message = data.decode().split(":")
+            client = Client.instances.get(int(client_id))
 
-        if client is None:
-            print(f"Client {client_id} not found")
-            continue
+            if client is None:
+                print(f"Client {client_id} not found")
+                continue
 
-        if address not in Client.get_udp_address():
-            print(f"Accepted UDP connection from {address}, new client {client.id} added to the list of clients.")
-            client.udp_address = address
-            continue
+            if address not in Client.get_udp_address():
+                client.udp_address = address
+                print(f"Accepted UDP connection from client {client_id} with  address {address}, saved to the server.")
+                continue
 
-        new_message = f"Client {client.id}:\n{message}"
-        print("UDP SERVER: ", new_message, "from", address)
-        client.send_to_all_clients(new_message.encode('utf-8') + b' from UDP', udp=True)
+            new_message = f"U{client.id}:\n{message}"
+
+            client.send_to_all_clients(new_message.encode('utf-8'), udp=True)
+
+    except KeyboardInterrupt:
+        print("Server UDP communication handler shutting down...")
+
+    finally:
+        udp_socket.close()
+        print("Server UDP communication handler ended")
+        sys.exit(0)
 
 
 def main():
     global host, port, max_users
-
+    print(f"Server listening on {host}:{port}")
     # Create a TCP socket
+    print("Server TCP starting...")
     tcp_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcp_server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # TODO, doit better
     tcp_server_socket.bind((host, port))
     tcp_server_socket.listen(max_users)
+    print("Server TCP started")
 
+    print("Server UDP starting...")
     udp_server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     udp_server_socket.bind((host, port))
+    print("Server UDP started")
 
     udp_communication_thread = threading.Thread(target=handle_udp_communication, args=(udp_server_socket,))
     udp_communication_thread.start()
 
-    print(f"Server listening on {host}:{port}")
-
+    print("-" * 100)
+    clients_threads = []
     try:
         while True:
             # Accept incoming connection
@@ -119,18 +139,40 @@ def main():
             if len(Client.instances) >= max_users:
                 client_socket.sendall("Server is full".encode())
                 client_socket.close()
+                print(f"New TCP connection from {client_address}, server is full.")
                 continue
             else:
                 client = Client(client_socket)
+                print(f"Accepted TCP connection from {client_address}, new client {client.id} added to the server.")
                 client_socket.sendall(f"Welcome to the server, client {client.id}".encode())
 
             client_thread = threading.Thread(target=client.handle_tcp_communication)
+            clients_threads.append(client_thread)
             client_thread.start()
 
+
     except KeyboardInterrupt:
+        print("-" * 170)
         print("Server shutting down")
+        exit_flag.set()
+        udp_server_socket.sendto(b"Server is shutting down", (host, port))
+        udp_communication_thread.join()
+
+        clients_sockets = Client.get_clients_sockets()
+
+        for client_socket in clients_sockets:
+            print(f"Connection closed by the server with  {client_socket}, server is shutting down...")
+            client_socket.shutdown(socket.SHUT_RDWR)
+
+        for client_thread in clients_threads:
+            client_thread.join()
+
+        tcp_server_socket.shutdown(socket.SHUT_RDWR)
+
         udp_server_socket.close()
         tcp_server_socket.close()
+        print("Server closed")
+        sys.exit(0)
 
 
 if __name__ == "__main__":
